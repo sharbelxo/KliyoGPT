02973229c256cdf0fb420a4f3bc01636158a7593
Sharbel <sharbelabousabha@outlook.com>
2022-12-23 18:19:27 +0200

    hi

--- client/script.js
+++ client/script.js
@@ -1,4 +1,4 @@
-import bot from './assets/KLIYOlogo.png'
+import bot from './assets/bot.svg'
 import user from './assets/user.svg'
 
 const form = document.querySelector('form')
@@ -52,7 +52,7 @@ function chatStripe(isAi, value, uniqueId) {
               <div class="profile">
                   <img 
                     src=${isAi ? bot : user} 
-                    alt="${isAi ? 'KLIYOlogo' : 'user'}" 
+                    alt="${isAi ? 'bot' : 'user'}" 
                   />
               </div>
               <div class="message" id=${uniqueId}>${value}</div>
